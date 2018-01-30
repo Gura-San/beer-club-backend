34393bcdbc61cb68debbcdc513a63206f025a6f5
Sandro Guraspasvili <gura.san@gmail.com>
2018-01-30 13:47:35 -0500

    connecting fronEnd to backEnd

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 const URL = 'mongodb://localhost/DBC'
 
-// mongoose.connect(URL, {useMongoClient: true})
-
 if (process.env.NODE_ENV == 'production') {
   mongoose.connect(process.env.MLAB_URL)
 } else {
